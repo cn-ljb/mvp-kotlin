b4faac11155bfcc3ac68ffc7efb7d0959117776d
cn_ljb <cn_ljb@sina.cn>
2017-07-19 18:07:03 +0800

    add my starred UI

--- app/src/main/java/com/ljb/mvp/kotlin/fragment/StarredFragment.kt
+++ app/src/main/java/com/ljb/mvp/kotlin/fragment/StarredFragment.kt
@@ -2,10 +2,13 @@ package com.ljb.mvp.kotlin.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ljb.mvp.kotlin.R
+import com.ljb.mvp.kotlin.adapter.StarredAdapter
+import kotlinx.android.synthetic.main.fragment_events.*
 
 /**
  * Created by L on 2017/7/19.
@@ -16,4 +19,14 @@ class StarredFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_starred, null)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        val manager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        recyclerview.layoutManager = manager
+        recyclerview.adapter = StarredAdapter(activity)
+    }
 }
