e234107cd29904d1c413cca8dc5e2eadc3d63d3a
cn_ljb <cn_ljb@sina.cn>
2017-07-19 18:48:02 +0800

    add  my followers UI

---
+++ app/src/main/java/com/ljb/mvp/kotlin/adapter/FollowersAdapter.kt
@@ -0,0 +1,27 @@
+package com.ljb.mvp.kotlin.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.ljb.mvp.kotlin.R
+
+/**
+ * Created by L on 2017/7/19.
+ */
+class FollowersAdapter(val mContext: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        return FollowersViewHolder(View.inflate(mContext, R.layout.item_followers, null))
+    }
+
+    override fun getItemCount(): Int {
+        return 30
+    }
+
+    class FollowersViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+
+}
--- app/src/main/java/com/ljb/mvp/kotlin/fragment/FollowersFragment.kt
+++ app/src/main/java/com/ljb/mvp/kotlin/fragment/FollowersFragment.kt
@@ -2,10 +2,13 @@ package com.ljb.mvp.kotlin.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ljb.mvp.kotlin.R
+import com.ljb.mvp.kotlin.adapter.FollowersAdapter
+import kotlinx.android.synthetic.main.fragment_events.*
 
 /**
  * Created by L on 2017/7/19.
@@ -13,5 +16,16 @@ import com.ljb.mvp.kotlin.R
 class FollowersFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_followers, null)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        val manager = GridLayoutManager(activity, 3)
+        recyclerview.layoutManager = manager
+        recyclerview.adapter = FollowersAdapter(activity)
     }
 }
